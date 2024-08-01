41e2ef6b5d918e660549986e3e8d47abe95751f8
Manisha Mali <82174253+M74-dot@users.noreply.github.com>
2024-08-01 22:15:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 const url = "https://jsonplaceholder.typicode.com/posts/1";
 
 //your JS code here. If required.
+const btn = document.querySelector('#btn');
+const outputDiv = document.querySelector('#output');
+
+async function fetchData(callback){
+	try{
+		const response = await fetch(url);
+		const data = await response.json();
+		callback(data.title);
+	}catch(error){
+		console.error('Error fetching data: ',error);
+	}
+}
+function displayMessage(msg){
+	outputDiv.textContent = msg;
+}
+btn.addEventListener('click',()=>{
+	fetchData(displayMessage)
+});
